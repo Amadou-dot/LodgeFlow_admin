1aafc0b36c57d80c156e372bd6f95dd211862cc2
Amadou-dot <89759636+Amadou-dot@users.noreply.github.com>
2025-08-16 23:21:46 -0600

    refactor: clean up navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,75 +1,67 @@
-"use client";
+'use client';
 
+import { Link } from '@heroui/link';
 import {
   Navbar as HeroUINavbar,
-  NavbarBrand,
   NavbarContent,
   NavbarMenu,
   NavbarMenuItem,
-  NavbarMenuToggle,
-} from "@heroui/navbar";
-import { Link } from "@heroui/link";
+  NavbarMenuToggle
+} from '@heroui/navbar';
 
-import { Logo } from "@/components/icons";
-import { ThemeSwitch } from "@/components/theme-switch";
-import { Avatar } from "@heroui/avatar";
+import { ThemeSwitch } from '@/components/theme-switch';
+import { Avatar } from '@heroui/avatar';
 import {
   Dropdown,
   DropdownItem,
   DropdownMenu,
   DropdownTrigger,
-} from "@heroui/dropdown";
+} from '@heroui/dropdown';
 
 export const Navbar = () => {
   const menuItems = [
-    { name: "Dashboard", href: "/" },
-    { name: "Cabins", href: "/cabins" },
-    { name: "Bookings", href: "/bookings" },
-    { name: "Guests", href: "/guests" },
-    { name: "Settings", href: "/settings" },
+    { name: 'Dashboard', href: '/' },
+    { name: 'Cabins', href: '/cabins' },
+    { name: 'Bookings', href: '/bookings' },
+    { name: 'Guests', href: '/guests' },
+    { name: 'Settings', href: '/settings' },
   ];
 
   return (
-    <HeroUINavbar isBordered maxWidth="2xl">
+    <HeroUINavbar isBordered maxWidth='2xl'>
       <NavbarContent>
-        <NavbarMenuToggle className="md:hidden" />
-        <NavbarBrand>
-          <Logo />
-          <p className="font-bold text-inherit ml-2">The Wild Oasis</p>
-        </NavbarBrand>
+        <NavbarMenuToggle className='md:hidden' />
       </NavbarContent>
 
-      <NavbarContent as="div" justify="end">
+      <NavbarContent as='div' justify='end'>
         <ThemeSwitch />
 
         <Dropdown>
           <DropdownTrigger>
             <Avatar
-              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-              className="cursor-pointer"
+              src='https://i.pravatar.cc/150?u=a042581f4e29026704d'
+              className='cursor-pointer'
             />
           </DropdownTrigger>
-          <DropdownMenu aria-label="Profile Actions" variant="flat">
+          <DropdownMenu aria-label='Profile Actions' variant='flat'>
             <DropdownItem
-              key="profile"
-              className="h-14"
-              textValue="Signed in as zoey@example.com"
-            >
-              <p className="font-semibold">Signed in as</p>
-              <p className="font-semibold">zoey@example.com</p>
+              key='profile'
+              className='h-14'
+              textValue='Signed in as zoey@example.com'>
+              <p className='font-semibold'>Signed in as</p>
+              <p className='font-semibold'>zoey@example.com</p>
             </DropdownItem>
-            <DropdownItem key="settings" textValue="My Settings">
+            <DropdownItem key='settings' textValue='My Settings'>
               My Settings
             </DropdownItem>
-            <DropdownItem key="help_and_feedback" textValue="Help & Feedback">
+            <DropdownItem key='help_and_feedback' textValue='Help & Feedback'>
               Help & Feedback
             </DropdownItem>
             <DropdownItem
-              key="logout"
-              className="text-danger"
-              color="danger"
-              textValue="Log Out"
-            >
+              key='logout'
+              className='text-danger'
+              color='danger'
+              textValue='Log Out'>
               Log Out
             </DropdownItem>
           </DropdownMenu>
@@ -80,11 +72,10 @@ export const Navbar = () => {
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={index}>
             <Link
-              color="foreground"
-              className="w-full"
+              color='foreground'
+              className='w-full'
               href={item.href}
-              size="lg"
-            >
+              size='lg'>
               {item.name}
             </Link>
           </NavbarMenuItem>
