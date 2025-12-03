1fbf434c772c99624d481f8b2ee4829f18e96fee
Amadou-dot <amadou.sseck@gmail.com>
2025-12-03 14:21:15 -0700

    chore: Remove ESLint configuration from next.config.js

--- next.config.js
+++ next.config.js
@@ -1,14 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    // ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
-        protocol: "https",
+        protocol: "https", 
         hostname: "images.unsplash.com",
         port: "",
         pathname: "/**",
